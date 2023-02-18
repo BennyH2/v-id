22b051cb4185214640c725ad6fb9c74b967d91ba
Bennett haynes <bennetthaynes1@icloud.com>
2023-02-18 11:37:22 -0600

    Adding Plugins

---
+++ config/filament-webhook-server.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+     *  Models that you want to be part of the webhooks options
+     */
+    'models' => [
+        \App\Models\User::class,
+        \App\Models\staff::class,
+        \App\Models\staff_prints::class,
+
+    ],
+    /*
+     */
+    'polling' => '10s',
+    'webhook' => [
+        'keep_history' => false,
+    ],
+];
