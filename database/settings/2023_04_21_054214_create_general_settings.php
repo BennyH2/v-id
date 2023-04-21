e0030ae99a774c03765353f43d405ee3368da816
Bennett haynes <bennetthaynes1@icloud.com>
2023-04-21 16:15:48 -0500

    first commit

--- database/settings/2023_04_21_054214_create_general_settings.php
+++ database/settings/2023_04_21_054214_create_general_settings.php
@@ -6,9 +6,9 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('FrontEnd.favicon', '');
-        $this->migrator->add('FrontEnd.terms_conditions', '');
-        $this->migrator->add('FrontEnd.privacy_policy', '');
-        $this->migrator->add('FrontEnd.brochure', '');
+        $this->migrator->add('FrontEnd.favicon', 'favicon/favicon.svg');
+        $this->migrator->add('FrontEnd.terms_conditions', '<h1>Terms & Conditions</h1>');
+        $this->migrator->add('FrontEnd.privacy_policy', '<h1>Privacy Policy</h1>');
+        $this->migrator->add('FrontEnd.brochure', 'brodchure/brodchure.pdf');
     }
 };
